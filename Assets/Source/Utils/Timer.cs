8ee965aa69d3431da0f538e5bce2f742fbda0ebe
Vova <docentkafedru@gmail.com>
2024-10-07 13:29:50 +0300

    added unitask lib and timer

---
+++ Assets/Source/Utils/Timer.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace Source.Utils
+{
+    public class Timer
+    {
+        public event Action<int> OnTick;
+        public event Action OnComplete;
+        public bool IsRunning { get; private set; }
+
+        private float _duration;
+        private CancellationTokenSource _cancellationTokenSource;
+
+        public Timer(float duration)
+        {
+            _duration = duration;
+            IsRunning = false;
+        }
+
+        
+        public async UniTask StartTimer()
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+            var token = _cancellationTokenSource.Token;
+
+            IsRunning = true;
+            int elapsedTime = 0;
+
+            try
+            {
+                while (elapsedTime < _duration)
+                {
+                    await UniTask.Delay(TimeSpan.FromSeconds(1), cancellationToken: token);
+                    elapsedTime++;
+                    OnTick?.Invoke(elapsedTime);
+                }
+
+                IsRunning = false;
+                OnComplete?.Invoke();
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.Log("Timer was forcefully stopped.");
+            }
+            finally
+            {
+                IsRunning = false;
+                
+                _cancellationTokenSource.Dispose();
+                _cancellationTokenSource = null;
+            }
+        }
+
+        public void StopTimer()
+        {
+            _cancellationTokenSource?.Cancel();
+        }
+
+        public void Reset(int timeDuration)
+        {
+            _duration = timeDuration;
+            IsRunning = false;
+            _cancellationTokenSource?.Cancel();
+            _cancellationTokenSource = null;
+        }
+    }
+
+}
