2591b4d221c42cae6095205e8a4b9f68118e6c6a
Vova <docentkafedru@gmail.com>
2024-10-07 12:24:45 +0300

    created a project

---
+++ Assets/Source/Application/Client.cs
@@ -0,0 +1,19 @@
+// using UnityEngine;
+//
+// namespace Application
+// {
+//     public class Client : MonoBehaviour
+//     {
+//         //private MonoService _monoService;
+//         private ControllerFactory _controllerFactory;
+//
+//         void Start()
+//         {
+//             // _monoService = new MonoService();
+//             // _monoService.Initialize();
+//             _controllerFactory = new ControllerFactory(_monoService);
+//             // GameplayController gameplayController = _controllerFactory.CreateController<GameplayController>();
+//             // gameplayController.Initialize();
+//         }
+//     }
+// }
